e391118511433e0e39e0d2d051dcfa781b4a7339
Arthur Fanti <arthur@skore.io>
2017-03-05 22:37:08 -0300

    toggling

--- app/components/addTodo.js
+++ app/components/addTodo.js
@@ -4,29 +4,45 @@ import ReactDOM from 'react-dom';
 import appStore from '../stores/app'
 
 let nextTodoId = 0
-const AddTodo = (props) => {
-  let { todos, ...rest } = props
-
-  return (
-    <div>
-      <button onClick={() => {
-        appStore.dispatch({
-          type: 'ADD_TODO',
-          text: 'Test',
-          id: nextTodoId++
-        })
-      }}>
-        Add Todo
-      </button>
-      <ul>
-        {todos.map(t =>
-          <li key={t.id}>
-            {t.text}
-          </li>
-        )}
-      </ul>
-    </div>
-  )
+class  AddTodo extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref={node=> {
+          this.input = node
+        }} />
+        <button onClick={() => {
+          appStore.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextTodoId++
+          })
+          this.input.value = ''
+        }}>
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos.map(t =>
+            <li key={t.id}
+              onClick={() => {
+                appStore.dispatch({
+                  type:'TOGGLE_TODO',
+                  id: t.id
+                })
+              }}
+              style={{
+                textDecoration:
+                t.completed ?
+                  'line-through' :
+                  'none'
+              }}>
+              {t.text}
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default AddTodo
